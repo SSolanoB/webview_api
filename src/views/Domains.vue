33ff6fad1093760b58d1f343ed4c7a035030e876
SSolanoB <s.solano586@uniandes.edu.co>
2020-07-06 22:38:32 -0500

    modifies view of domains, now it uses axios and requests info to the API

--- src/views/Domains.vue
+++ src/views/Domains.vue
@@ -2,12 +2,12 @@
   <div class="domains">
     <h1>List of all domains searched by customers</h1>
     <p>
-      In the links bellow you can find our two services! Go ahead and try them.
+      In the next card you will find all the domains! Thank you for using our service.
     </p>
     <b-container class="bv-row">
       <b-row>
         <b-col>
-          <b-card tag="article" style="max-width: 20rem;" class="mb-2">
+          <b-card tag="article" class="mb-2">
             <img alt="Domain logo" style="width:60px" class="card-img-top" src="../assets/history-24px.svg">
             <b-card-body>
               <b-card-title>
@@ -17,15 +17,55 @@
                 This will give you all the domains that have been requested through our API.
               </b-card-text>
 
-              <b-button to="/domains" variant="outline-primary">All domains!</b-button>
+              <ul class="list-group row" v-if="domains && domains.length">
+                <li class="list-group-item col-xs-6 col-sm-4 col-md-3" v-for="domain of domains" v-bind:key="domain.id">
+                  <p>{{domain.domain}}</p>
+                </li>
+              </ul>
+              <br>
+              <b-button to="/domains" onClick="window.location.reload();" variant="outline-primary">Refresh list</b-button>
             </b-card-body>
           </b-card>
         </b-col>
       </b-row>
     </b-container>
-    
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      domains: [],
+      errors_d: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+
+    axios.get(`http://localhost:3000/domains`)
+    .then(response => {
+      this.domains = response.data.items
+      console.log(this.domains)
+    })
+    .catch(e_d => {
+      this.errors_d.push(e_d)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
